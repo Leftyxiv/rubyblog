581edac3ce0515b5dab2efc55c616d710ff1a92d
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-27 12:03:11 -0700

    added a partial for the messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to @article
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params) 
       flash[:notice] = "Article was successfully updated"
       redirect_to @article
     else
@@ -42,5 +42,9 @@ class ArticlesController < ApplicationController
   private
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
