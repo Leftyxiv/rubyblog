0075c14e5161f265a1681f3990d3aaff81876c8c
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-27 10:26:01 -0700

    added a create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,12 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 
