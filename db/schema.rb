a9b837492fd52d2a450d858f972c940025b1746b
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-28 06:32:40 -0700

    added users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_27_143114) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_133049) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "just_because_timestamp"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
