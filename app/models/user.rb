3e53070b56abcd9bc07b29e63e1c34df5d244aad
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-28 07:50:31 -0700

    added a sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } 
   has_many :articles, dependent: :destroy
   validates :username, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "only allows letters and numbers" }
   validates :password, presence: true, length: { minimum: 6, maximum: 50 }, confirmation: true
   validates :email, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "must be a valid email address" }
+  has_secure_password  
+end
