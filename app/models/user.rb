696e04e444234d9cf285a6669a43b0aed60a9561
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-28 06:54:08 -0700

    made the model associations and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
-end
+  has_many :articles, dependent: :destroy
+  validates :username, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "only allows letters and numbers" }
+  validates :password, presence: true, length: { minimum: 6, maximum: 50 }, confirmation: true
+  validates :email, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "must be a valid email address" }
