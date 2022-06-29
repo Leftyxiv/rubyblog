2912d01cd2577be4a7a24d3f22d0ec03f9f68e50
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-29 10:23:31 -0700

    abstracted out some logic to create a helper method in thbe users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,13 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
     image_tag(gravatar_url, alt: user.username)
   end
-  
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
