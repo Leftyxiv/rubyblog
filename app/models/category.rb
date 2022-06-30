3f00f50a8c87669bc9a4a0292d69e7e0d28ad279
Manny Ledoux <manny.ledoux@checkr.com>
2022-06-30 05:26:32 -0700

    added some tests for the category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-
+  has_many :articles, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "only allows letters and numbers" }
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,7 +3,26 @@ require 'test_helper'
 class CategoryTest < ActiveSupport::TestCase
   test "category name should be present" do
     @category = Category.new(name: "Veggies")
-    assert_not category.valid?
-    assert category.errors[:name].any?
+    assert @category.valid?
+  end
+
+  test "category name should be unique" do
+    @category = Category.new(name: "Veggies")
+    @category.save
+    @category2 = Category.new(name: "Veggies")
+    assert_not @category2.valid?
+    assert @category2.errors[:name].any?
+  end
+
+  test "category name should be at least 3 characters" do
+    @category = Category.new(name: "Ve")
+    assert_not @category.valid?
+    assert @category.errors[:name].any?
+  end
+
+  test "category name should be at most 50 characters" do
+    @category = Category.new(name: "Veggies" * 20)
+    assert_not @category.valid?
+    assert @category.errors[:name].any?
   end
 end
